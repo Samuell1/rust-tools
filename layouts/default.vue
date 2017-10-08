23d8116544d720a3b4f93a0ecf3ed3d912d5969c
Samuell <samuell.patro@gmail.com>
2017-10-08 12:53:50 +0200

    Added footer

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="container">
     <nuxt/>
+    <footer>
+      Created by <a href="https://github.com/Samuell1">Samuell</a>. Images are used from Rust Wiki and Rustlabs.com
+    </footer>
   </div>
 </template>
 
@@ -11,5 +14,10 @@
   margin: auto;
   padding: 0 16px;
   min-height: 100vh;
+}
+
+footer {
+  font-size: 12px;
+  margin: 20px 0;
 }
 </style>
