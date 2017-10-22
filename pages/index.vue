bb18b959de2869aaf2516e89ece46f392dc928dc
Samuell <samuell.patro@gmail.com>
2017-10-22 10:49:11 +0200

    Added last update

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,23 @@
 <template>
   <div>
-    <div class="search">
-      <input type="text" v-model="search" placeholder="Search...">
-      <spinner v-if="search && loading > 0"></spinner>
-    </div>
+    <header>
+      <img src="/box.png" alt="Rust Loot Tables">
+      <div>
+        <h1>Rust Loot Tables</h1>
+        <p>List of loot tables for various crates, boxes, barrels, piles.</p>
+      </div>
+      <div class="search">
+        <div class="search-input">
+          <input type="text" v-model="search" placeholder="Search item...">
+          <spinner v-if="search && loading > 0"></spinner>
+        </div>
+        <button @click="orderByChance = !orderByChance" class="order" :class="{ 'active' : orderByChance }">Order by Chance</button>
+      </div>
+    </header>
     <div class="info">
       <small>(Press on list of items and move left, right to move long list of items.)</small>
-      <div class="order">
-        <button @click="orderByChance = !orderByChance" :class="{ 'active' : orderByChance }">Order by Chance</button>
+      <div class="lastupdate">
+        Last updated {{ lastUpdate || '...' }}
       </div>
     </div>
     <template v-if="search || loading === 0">
@@ -25,6 +35,9 @@
 </template>
 
 <script>
+import timeago from 'timeago.js'
+
+import allChangelogs from '~/apollo/allChangelogs.gql'
 import allCrates from '~/apollo/allCrates.gql'
 
 import LootList from '~/components/LootList'
@@ -43,6 +56,11 @@ export default {
     LootList,
     Spinner
   },
+  computed: {
+    lastUpdate () {
+      return this.lastChange && timeago().format(this.lastChange.date)
+    }
+  },
   apollo: {
     allCrates: {
       query: allCrates,
@@ -57,6 +75,10 @@ export default {
           this.loading += countModifier
         })
       }
+    },
+    lastChange: {
+      query: allChangelogs,
+      update: (data) => data.allChangelogs[0]
     }
   },
   methods: {
@@ -72,8 +94,9 @@ export default {
 @import 'assets/variables.scss';
 
 header {
-  margin: 20px 0 0;
+  margin: 20px 0;
   display: flex;
+  flex-flow: wrap;
   align-items: center;
   img {
     max-width: 60px;
@@ -126,45 +149,60 @@ header {
 
 .info {
   display: flex;
-  .order {
+  flex-flow: wrap;
+  .lastupdate {
     margin-left: auto;
-    button {
-      background: $secondaryBackground;
-      cursor: pointer;
-      border: 0px;
-      font-size: 11px;
-      padding: 4px 6px;
-      color: $primaryText;
-      border-radius: 3px;
-      &.active {
-        background: $primary;
-        color: $white;
-      }
-    }
+    font-size: 12px;
+    opacity: .6;
   }
 }
 
 .search {
-  position: relative;
-  margin: 16px 0;
-  max-width: 300px;
-  input {
-    background: $secondaryBackground;
-    border: 0px;
-    padding: 8px 26px 8px 12px;
+  display: flex;
+  margin-left: 20px;
+  max-width: 500px;
+  width: 100%;
+  @media screen and (max-width: 1024px) {
+    margin: 16px 0 0;
+  }
+  .search-input {
+    position: relative;
+    max-width: 300px;
     width: 100%;
-    color: $secondaryText;
-    &:focus {
-      background: lighten($secondaryBackground, 5%);
+    input {
+      background: $secondaryBackground;
+      border: 0px;
+      border-radius: 3px;
+      padding: 8px 26px 8px 12px;
+      width: 100%;
+      color: $secondaryText;
+      &:focus {
+        background: lighten($secondaryBackground, 5%);
+      }
+    }
+    .spinner {
+      position: absolute;
+      top: 9px;
+      right: 6px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
     }
   }
-  .spinner {
-    position: absolute;
-    top: 9px;
-    right: 6px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
+
+  .order {
+    background: $secondaryBackground;
+    margin-left: 8px;
+    cursor: pointer;
+    border: 0px;
+    font-size: 11px;
+    padding: 4px 8px;
+    color: $primaryText;
+    border-radius: 3px;
+    &.active {
+      background: $primary;
+      color: $white;
+    }
   }
 }
 </style>
