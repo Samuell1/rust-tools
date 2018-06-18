e3b9def4c50e5973e29ab0577273b5a019a28d9b
Samuell <samuell.patro@gmail.com>
2018-06-18 14:54:32 +0200

    Add find api

---
+++ server/api/find.js
@@ -0,0 +1,48 @@
+import { Router } from 'express'
+import { GraphQLClient } from 'graphql-request'
+
+const router = Router()
+const client = new GraphQLClient('https://api.graph.cool/simple/v1/cj8g20g6q030q0156airzo84g')
+
+router.get('/find/:query', async ({ params }, res, next) => {
+  if (!params.query || params.query.length < 2) {
+    return res.send(`Please write name of a item!`)
+  }
+
+  const search = await client.request(`
+      query allLoots($search: String) {
+        allLoots(filter: {OR: [{dataId_contains: $search}, {name_starts_with: $search}]}, first: 1) {
+          name
+        }
+      }
+    `, {
+      search: params.query
+    })
+
+  if (!search.allLoots) {
+    return res.send(`Nothing founded :(`)
+  }
+
+  const { allLoots } = await client.request(`
+      query allLoots($search: String) {
+        allLoots(filter: {OR: [{dataId_contains: $search}, {name_starts_with: $search}]}, first: 2) {
+          name
+          percentage
+          crate {
+            name
+          }
+        }
+      }
+    `, {
+      search: search.allLoots[0].name
+    })
+
+  let items = []
+  allLoots.forEach(item => {
+    items.push(`${item.name} > ${item.crate.name}(${item.percentage}%)`)
+  })
+
+  res.send(`${items.join(', ')}`)
+})
+
+export default router
--- server/api/index.js
+++ server/api/index.js
@@ -1,10 +1,12 @@
 import { Router } from 'express'
 
 import decay from './decay'
+import find from './find'
 
 const router = Router()
 
 // Routes
 router.use(decay)
+router.use(find)
 
 export default router
